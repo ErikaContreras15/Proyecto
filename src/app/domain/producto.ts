4d1d8808d986210e794582d3752349f916db7be2
ErikaContreras15 <econtrerasz@est.ups.edu.ec>
2024-01-17 08:17:45 -0500

    2

--- src/app/domain/producto.ts
+++ src/app/domain/producto.ts
@@ -1,1 +1,14 @@
 //sdfsdfsdfsdfsd
+export class Produc {
+  
+    nombre:string='';
+    descripcion:string ='';
+    disponibilidad:string='';
+    categoria:string ='';
+    stock:number = 0 ;
+    talla:string ='';
+    color:string ='';
+    precio:number = 0.00;
+    
+
+}
